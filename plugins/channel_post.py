4cbfe0bcfa913dd02d27e152cb9610500cafdd0d
shadow-Senpai0 <132126340+shadow-Senpai0@users.noreply.github.com>
2023-08-07 20:06:26 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -68,7 +68,7 @@ async def channel_post(client: Client, message: Message):
     )
 
     await reply_text.edit(
-        f"<b>Link Sharing File Berhasil Di Buat :</b>\n\n{link}",
+        f"<b>Here Your File Link  :</b>\n\n{link}",
         reply_markup=reply_markup,
         disable_web_page_preview=True,
     )
